1a88bdc3cbc583a866519a0c0664801d891536a7
DL1793 <mihainiculescuc@gmail.com>
2026-02-09 23:12:43 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,54 @@
 use std::io::{self, Write};
 use std::process;
 
+enum BuiltIns {
+    Type,
+    Echo,
+    Exit
+}
+
+impl BuiltIns {
+    fn from_command(cmd: &str) -> Option<BuiltIns> {
+        match cmd {
+            "echo" => Some(BuiltIns::Echo),
+            "exit" => Some(BuiltIns::Exit),
+            "type" => Some(BuiltIns::Type),
+            _ => None,
+        }
+    }
+}
+
 fn main() {
     loop {
-        let mut command = String::new();
+        
         print!("$ ");
         io::stdout().flush().unwrap();
+
+        let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
         
-       if command.trim() == "exit" {
-        process::exit(0)
-       }
+        let mut parts = command.trim().splitn(2, ' ');
 
-        if command.starts_with("echo") {    
-            if let Some(args) = command.trim().strip_prefix("echo ") {
-                println!("{}", args);
-       }
-       }
-       else {
-        println!("{}: command not found", command.trim())
-       }
+        let cmd_str = parts.next().unwrap_or("");
+        let args = parts.next().unwrap_or("");
+
+        match BuiltIns::from_command(cmd_str) {
+            Some(BuiltIns::Exit) => process::exit(0),
+            Some(BuiltIns::Echo) => println!("{}", args),
+            Some(BuiltIns::Type) => {
+                match BuiltIns::from_command(args) {
+                    Some(_) => {
+                        println!("{} is a shell builtin", args);
+                    }
+                    None => {
+                        println!("{}: not found", args)
+                    }
+                }
+            }
+            None => {
+                println!("{} not found", cmd_str)
+            }
+        }
+
     }
 }
