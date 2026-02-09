d9a59a3588b55ed36cfbcbe75d071c3f5cdbccb6
DL1793 <mihainiculescuc@gmail.com>
2026-02-09 22:19:02 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    let mut command = String::new();
+    
 
     loop {
+        let mut command = String::new();
         print!("$ ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut command).unwrap();
