ce848517b5a14012297f28b240c82a10a9aa1e5b
DL1793 <mihainiculescuc@gmail.com>
2026-02-09 23:35:36 +0200

    codecrafters submit [skip ci]

--- src/find_exec.rs
+++ src/find_exec.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 pub fn find_executable(cmd: &str) -> Option<String> {
     let path_var = env::var("PATH").ok()?;
@@ -7,7 +7,11 @@ pub fn find_executable(cmd: &str) -> Option<String> {
         let full_path = path.join(cmd);
 
         if full_path.is_file() {
-            return full_path.to_str().map(|s| s.to_string());
+            if let Ok(metadata) = fs::metadata(&full_path) {
+                if metadata.permissions().mode() & 0o111 != 0 {
+                    return full_path.to_str().map(|s| s.to_string());
+                }
+            }
         }
     }
 
